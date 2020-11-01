2ab57c6f8c39b1e82464702d4bdc46f252e90c93
Jared Seefried <jared.seefried@yahoo.com>
2020-11-01 16:40:52 -0700

    Updated burger_controller file to give the router a variable and export it to the server file. Also, gave it a GET route

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -2,6 +2,18 @@ const express = require("express");
 const burger = require("../models/burger");
 
 const app = express();
+const router = express.Router();
+
+//GET ROUTE
+router.get("/", (error, response) =>{
+    burger.all((data)=>{
+        const burgerObj = {
+            burgers: data
+        }
+        console.log(burgerObj);
+        response.render("index", burgerObj)
+    })
+})
 
 
-// module.exports = router
+module.exports = router
