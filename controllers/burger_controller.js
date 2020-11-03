d97a6d68a23ec985545613136b352bb5d8cd4558
Jared Seefried <jared.seefried@yahoo.com>
2020-11-02 17:09:50 -0700

    Added the routers

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,19 +1,58 @@
-const express = require("express"); 
+const express = require("express");
 const burger = require("../models/burger");
 
-const app = express();
 const router = express.Router();
 
 //GET ROUTE
-router.get("/", (error, response) =>{
-    burger.all((data)=>{
-        const burgerObj = {
+router.get("/", (request, response) => {
+    burger.selectAll((data) => {
+        const hbsObject = {
             burgers: data
         }
-        console.log(burgerObj);
-        response.render("index", burgerObj)
+        console.log(hbsObject);
+        response.render("index", hbsObject)
     })
 })
 
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    cat.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router
