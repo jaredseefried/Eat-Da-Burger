58fe7995142691b46d3faac916323ff7256de502
Jared Seefried <jared.seefried@yahoo.com>
2020-11-02 21:29:14 -0700

    Created the handlebars template with working expressions and added some styling

--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,25 @@
 const orm = require("../config/orm")
 
 const burger = {
-    selectAll: function(cb){
-        orm.selectAll("burgers", function(res){
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
             cb(res)
         })
+    },
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res); 
+        });
+    },
+    delete: function (condition, cb) {
+        orm.delete("burgers", condition, function (res) {
+            cb(res);
+        });
     }
 }
 module.exports = burger;
