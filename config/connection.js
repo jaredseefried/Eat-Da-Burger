80d65604ac501421eccbc16a270cccdc81c4f5cb
Jared Seefried <jared.seefried@yahoo.com>
2020-11-02 22:37:14 -0700

    fixing favicon issue

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306, // ALWAYS 3306
-    user: "root",
-    password: "Welcome123",
-    database: "burgers_db",
-});
+const connection;
+
+if( process.env.JAWS_URL){
+    connection = mysql.createConnection(process.env.JAWS_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306, // ALWAYS 3306
+        user: "root",
+        password: "Welcome123",
+        database: "burgers_db",
+    });
+}
 
 connection.connect(function (error) {
     if (error) throw error;
